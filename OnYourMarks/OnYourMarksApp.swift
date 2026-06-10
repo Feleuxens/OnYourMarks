1184e3552acf2cc2c52030a1edfa55a4e3e475bf
Feleuxens <felixgrueb@icloud.com>
2026-06-11 00:05:37 +0200

    Initial Commit

---
+++ OnYourMarks/OnYourMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnYourMarksApp.swift
+//  OnYourMarks
+//
+//  Created by Felix on 11.06.26.
+//
+
+import SwiftUI
+
+@main
+struct OnYourMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnYourMarksTests/OnYourMarksTests.swift
@@ -0,0 +1,19 @@
+//
+//  OnYourMarksTests.swift
+//  OnYourMarksTests
+//
+//  Created by Felix on 11.06.26.
+//
+
+import Testing
+@testable import OnYourMarks
+
+struct OnYourMarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
